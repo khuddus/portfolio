9481feeee09668a8f662f2df3ee425504747a065
Abdul Khuddus <khuddus1@gmail.com>
2019-06-24 11:57:53 +0530

    Updates

---
+++ precache-manifest.88a356577a93910b8cfdb56ef0447668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09db694dce532e4b2de06b5284be520d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a8c6da09dfd2f1f0ea2b",
+    "url": "/portfolio/static/css/main.f5d87644.chunk.css"
+  },
+  {
+    "revision": "554e9198795af90f2f1d",
+    "url": "/portfolio/static/js/2.463c3de0.chunk.js"
+  },
+  {
+    "revision": "a8c6da09dfd2f1f0ea2b",
+    "url": "/portfolio/static/js/main.ad49b42f.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "6d0457358506311d06af540d40d6a059",
+    "url": "/portfolio/static/media/train.6d045735.png"
+  }
+]);
