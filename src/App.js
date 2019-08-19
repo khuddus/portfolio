7eb1957d3da6db580456d7fe69d714b844f2e94f
Abdul Khuddus <khuddus1@gmail.com>
2019-08-19 16:45:31 +0530

    Latest code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
 } from 'react-transition-group';
 import logo from './images/train.png';
 import './App.css';
+import './css/icons.css';
 import Introduction from './pages/Introduction';
 import Philosophy from './pages/Philosophy';
 import Skills from './pages/Skills';
@@ -11,9 +12,11 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 function App() {
   const [count, setCount] = useState(0);
-  const [showMessage, setShowMessage] = useState(true);
+  const [showPage, setShowPage] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
   const [trainMargin, setTrainMargin] = useState(0);
-
+  const [menuStatus, setMenuStatus] = useState('close');
+ const steamMargin = 13;
   const pages = [
     Introduction,
     Philosophy,
@@ -21,28 +24,33 @@ function App() {
     Projects,
     Contact
   ]
-  
-  const changeClass = () => {
-    setShowMessage(!showMessage);
+
+  const changeClass = (index) => {
+    if(index !== currentPage){
+      setCurrentPage(index);   
+      setShowPage(!showPage);
+  }
+    menuButtonToggle();
 
   };
- 
+const menuButtonToggle = ()=>setMenuStatus(menuStatus === 'open' ? 'close' : 'open');
   return (
-    <div className="App"  onClick={changeClass}  >
+    <div className="App"    >
       <div className='pageContainer'>
         <CSSTransition
-          in={showMessage}
+          in={showPage}
           timeout={300}
           classNames="container"
           onExited={() => {
             const pageWidth = 80;
             const pagesCount = pages.length;
             const distancePerEachPage = parseInt(pageWidth / (pagesCount - 1));
-            
-            setTrainMargin(((count + 1) % pagesCount) * distancePerEachPage);
-            setCount((count + 1) % pages.length);
 
-            setShowMessage(!showMessage);
+            setTrainMargin(((count + 1) % pagesCount) * distancePerEachPage);
+            
+            setCount(currentPage);
+
+            setShowPage(!showPage);
 
           }}
           unmountOnExit
@@ -51,22 +59,17 @@ function App() {
         </CSSTransition>
       </div>
       <footer className="bg-light">
-        <CSSTransition
-          in={showMessage}
-          timeout={300}
-          classNames="train"
-         
-          unmountOnExit
-        >
-<div>
-<div class="steam" id="steam-one"  style={{ marginLeft: (trainMargin + 12) +  'vw' }}></div>
-  <div class="steam" id="steam-two" style={{ marginLeft: (trainMargin + 12) + 'vw' }}></div>
-  <div class="steam" id="steam-three" style={{ marginLeft: (trainMargin + 12) + 'vw' }}></div>
-  <div class="steam" id="steam-four" style={{ marginLeft: (trainMargin + 12) + 'vw' }}></div>
-   <img src={logo} onClick={changeClass} className="train" style={{ marginLeft: trainMargin + 'vw' }} alt="logo" />
-
-</div>
-                 </CSSTransition>
+      <nav id="bt-menu" className={"bt-menu bt-menu-" + menuStatus}>
+				<a href="#" className="bt-menu-trigger" onClick={menuButtonToggle}><span>Menu</span></a>
+				<ul>
+					<li><a href="#"  onClick={()=>{changeClass(0)}} className="bt-icon icon-user-outline">About</a></li>
+					<li><a href="#"  onClick={()=>{changeClass(1)}} className="bt-icon icon-sun">Skills</a></li>
+					<li><a href="#"  onClick={()=>{changeClass(2)}} className="bt-icon icon-windows">Work</a></li>
+					<li><a href="#"  onClick={()=>{changeClass(3)}} className="bt-icon icon-speaker">Blog</a></li>
+					<li><a href="#"  onClick={()=>{changeClass(4)}} className="bt-icon icon-star">Clients</a></li>
+					<li><a href="#"  onClick={()=>{changeClass(5)}} className="bt-icon icon-bubble">Contact</a></li>
+				</ul>
+			</nav>
       </footer>
     </div>
   );
